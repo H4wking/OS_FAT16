0582a1cf9a97537d9c88a8a5828d7cc94c26c479
H4wking <funnyman20062001@gmail.com>
2020-11-11 20:04:06 +0200

    output formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <iomanip>
+#include <string>
 
 struct Boot_sector {
     uint16_t bytes_per_sector;
@@ -15,8 +17,6 @@ struct Boot_sector {
 struct File {
     char name_extension[11];
     uint16_t size;
-//    uint16_t creation_time;
-//    uint16_t creation_date;
     uint16_t modification_time;
     uint16_t modification_date;
     uint16_t attribute;
@@ -40,7 +40,6 @@ int main(int argc, char **argv) {
 
     Boot_sector bs;
 
-
     read_img(&bs.bytes_per_sector, img, 11, 2);
     read_img(&bs.sectors_per_cluster, img, 13, 1);
     read_img(&bs.reserved_sector_count, img, 14, 2);
@@ -49,37 +48,85 @@ int main(int argc, char **argv) {
     read_img(&bs.table_size_in_sectors, img, 22, 2);
     read_img(&bs.signature, img, 510, 2);
 
+    std::cout << "--------BOOT SECTOR--------" << std::endl;
 
-    std::cout << bs.bytes_per_sector << std::endl;
-    std::cout << bs.sectors_per_cluster << std::endl;
-    std::cout << bs.reserved_sector_count << std::endl;
-    std::cout << bs.table_count << std::endl;
-    std::cout << bs.root_entry_count << std::endl;
-    std::cout << bs.table_size_in_sectors << std::endl;
-    std::cout << bs.signature << std::endl;
+    std::cout << "Sector size in bytes: " << bs.bytes_per_sector << std::endl;
+    std::cout << "Number of sectors is cluster: " << bs.sectors_per_cluster << std::endl;
+    std::cout << "FAT copies count: " << bs.table_count << std::endl;
+    std::cout << "FAT size in sectors: " << bs.table_size_in_sectors << std::endl;
+    std::cout << "FAT size in bytes: " << bs.table_size_in_sectors * bs.bytes_per_sector << std::endl;
+    std::cout << "Number of root directory entries: " << bs.root_entry_count << std::endl;
+    std::cout << "Size of root directory in bytes: " << bs.root_entry_count * 32 << std::endl;
+    std::cout << "Number of reserved sectors: " << bs.reserved_sector_count << std::endl;
+    std::cout << "Correct signature: " << ((bs.signature == 0xAA55) ? "true" : "false") << std::endl << std::endl;
 
     int offset = bs.reserved_sector_count * bs.bytes_per_sector +
                  bs.table_count * bs.table_size_in_sectors * bs.bytes_per_sector;
 
-    std::cout << offset << std::endl;
+    std::cout << "--------ROOT DIRECTORY--------" << std::endl;
 
     for (int i = 0; i < bs.root_entry_count; i++) {
         File f{};
 
         read_img(&f.name_extension, img, offset, 11);
         read_img(&f.size, img, offset + 28, 4);
-//        read_img(&f.creation_time, img, offset + 14, 2);
-//        read_img(&f.creation_date, img, offset + 16, 2);
         read_img(&f.modification_time, img, offset + 22, 2);
         read_img(&f.modification_date, img, offset + 24, 2);
         read_img(&f.attribute, img, offset + 11, 1);
         read_img(&f.first_cluster_address, img, offset + 26, 2);
 
+        if (f.name_extension[0] == 0x00) {
+            break;
+        }
 
+        std::string attr = "";
+        if (f.attribute & 0x01) {
+            attr += "Read-only   ";
+        }
+        if (f.attribute & 0x02) {
+            attr += "Hidden file   ";
+        }
+        if (f.attribute & 0x04) {
+            attr += "System file   ";
+        }
+        if (f.attribute & 0x08) {
+            attr += "Volume label   ";
+        }
+        if (f.attribute & 0x0f) {
+            attr += "Long file name   ";
+        }
+        if (f.attribute & 0x10) {
+            attr += "Directory   ";
+        }
+        if (f.attribute & 0x20) {
+            attr += "Archive   ";
+        }
 
+        std::string name = f.name_extension;
 
-        std::cout << f.name_extension << "  " << f.size << "  " << f.modification_time << "  " << f.modification_date
-        << " " << f.attribute << " " << f.first_cluster_address << std::endl;
+        if (name[8] != ' ') {
+            name = name.substr(0, 8) + "." + name.substr(8, 3) + " ";
+        } else {
+            name += "  ";
+        }
+
+        std::cout << name << "  ";
+
+        if (f.attribute & 0x10) {
+            std::cout << std::left << std::setfill(' ') << std::setw(8) << "DIR";
+        } else {
+            std::cout << std::left << std::setfill(' ') << std::setw(8) << f.size;
+        }
+
+        std::cout << std::right << std::setfill('0') << std::setw(2) << (f.modification_date & 0b11111) << "-" <<
+                     std::setfill('0') << std::setw(2) << ((f.modification_date >> 5) & 0b1111) << "-" <<
+                     ((f.modification_date >> 9) & 0b1111111) + 1980 << "  ";
+
+        std::cout << std::setfill('0') << std::setw(2) << ((f.modification_time >> 11) & 0b11111) << ":" <<
+                     std::setfill('0') << std::setw(2) << ((f.modification_time >> 5) & 0b111111) << ":" <<
+                     std::setfill('0') << std::setw(2) << (f.modification_time & 0b11111) * 2 << "  ";
+
+        std::cout << std::left << std::setfill(' ') << std::setw(70) << attr << f.first_cluster_address << std::endl;
 
         offset += 32;
     }
